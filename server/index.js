88e63057a7c3ad0669abc912dc0d26e77b6f313a
Artem Kholodenko <artemk@gmail.com>
2024-04-15 17:32:47 -0700

    cleaning up commented out code

--- server/index.js
+++ server/index.js
@@ -23,15 +23,3 @@ app.get("/*", function (req, res) {
 });
 
 app.listen(process.env.PORT || 7070);
-
-// const express = require('express')
-// const app = express()
-// const port = 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
